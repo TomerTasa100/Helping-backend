901d4a301c985a01c1b1ee2eccc481875d0483e4
TomerTasa100 <tomertasa1511@gmail.com>
2025-11-19 15:01:16 +0200

    fastapi fix

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# 1. Define the Shape of the Data (The "Schema")
-# This tells Python: "Expect a JSON with a username and password"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class LoginRequest(BaseModel):
     username: str
     password: str
@@ -13,15 +20,11 @@ class LoginRequest(BaseModel):
 def read_root():
     return {"status": "Online"}
 
-# 2. The Login Logic
 @app.post("/login")
 def login(data: LoginRequest):
-    # Print what we received (so you can see it in the PyCharm terminal)
     print(f"Received login attempt: {data.username}")
 
-    # SIMPLE LOGIC (We will connect a real database later)
     if data.username == "admin" and data.password == "123456":
         return {"status": "success", "message": "Welcome back, Admin!"}
     else:
-        # If wrong, send a 401 Error
         raise HTTPException(status_code=401, detail="Wrong username or password")
